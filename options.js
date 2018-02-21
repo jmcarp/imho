46f9196c38a89426a3f9b78bc3e5a73ec081a81b
Joshua Carp <jm.carp@gmail.com>
2018-02-20 23:53:04 -0500

    Prompt to tweet blacklist on save.

--- options.js
+++ options.js
@@ -21,8 +21,35 @@ const KNOWN_AUTHORS = [
   'Thomas L. Friedman'
 ];
 
+const TWITTER_MAP = {
+  'Bari Weiss': 'bariweiss',
+  'Bret Stephens': 'bretstephensnyt',
+  'Charles M. Blow': 'CharlesMBlow',
+  'Gail Collins': 'gailcollins',
+  'David Brooks': 'nytdavidbrooks',
+  'David Leonhardt': 'DLeonhardt',
+  'Frank Bruni': 'FrankBruni',
+  'Maureen Dowd': 'maureendowd',
+  'Michelle Goldberg': 'michelleinbklyn',
+  'Nicholas Kristof': 'NickKristof',
+  'Paul Krugman': 'paulkrugman',
+  'Roger Cohen': 'NYTimesCohen',
+  'Ross Douthat': 'DouthatNYT',
+  'Thomas L. Friedman': 'tomfriedman'
+};
+
+const TWEET_URL = 'https://twitter.com/intent/tweet';
+
 const union = (a, b) => {
   return new Set(Array.from(a).concat(Array.from(b)));
+};
+
+const toSentence = (items) => {
+  if (items.length <= 2) {
+    return items.join(' and ');
+  }
+  const last = items.length > 3 ? `${items.length - 2} more` : items.slice(2);
+  return items.slice(0, 2).concat(`and ${last}`).join(', ');
 };
 
 const saveOptions = async (e) => {
@@ -34,6 +61,21 @@ const saveOptions = async (e) => {
     return option.value;
   });
   await storage.set({blacklist: blacklist});
+
+  updateTweet(blacklist);
+};
+
+const updateTweet = (blacklist) => {
+  const tweetNode = document.querySelector('#tweet');
+  if (blacklist.length === 0) {
+    tweetNode.style['display'] = 'none';
+  } else {
+    tweetNode.style['display'] = 'block';
+    const handles = blacklist.map(item => (TWITTER_MAP[item] ? `@${TWITTER_MAP[item]}` : item));
+    const tweetMessage = `I'm blocking ${toSentence(handles)} on @nytopinion with @imhoext`;
+    tweetNode.querySelector('a').setAttribute('href', `${TWEET_URL}?text=${tweetMessage}`)
+    tweetNode.querySelector('.tweet-message').textContent = tweetMessage;
+  }
 };
 
 const loadOptions = async () => {
@@ -60,6 +102,8 @@ const loadOptions = async () => {
     option.appendChild(document.createTextNode(author));
     select.appendChild(option);
   });
+
+  updateTweet(Array.from(blacklist));
 
   document.querySelector('#save').addEventListener('click', saveOptions);
 };
